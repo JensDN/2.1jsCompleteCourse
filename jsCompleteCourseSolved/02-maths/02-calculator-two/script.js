2e8cdc19fdfcb360082abae419231d192e724baa
JensDN <jens.deneef@gmail.com>
2019-12-17 16:48:11 +0100

    Switsch case van sali overgenomen, maar ik versta alle elementen zoals this.id de switch etc

--- jsCompleteCourseSolved/02-maths/02-calculator-two/script.js
+++ jsCompleteCourseSolved/02-maths/02-calculator-two/script.js
@@ -1,61 +1,28 @@
-    /* becode/javascript
- *
- * /02-maths/02-calculator-two/script.js - 2.2: calculatrice (2)
- *
- * coded by leny@BeCode
- * started at 26/10/2018
- */
 
-// NOTE: don't focus on the existing code structure for now.
-// You will have time to focus on it later.
+function test(test) {
+    let number1 = document.getElementById ("op-one" ).value;
+    let number2 = document.getElementById ("op-two" ).value ;
+    let result;
 
-function test(expression) {
-    let variable1 = document.getElementById("one").value;
-    let variable2 = document.getElementById("two").value;
-    let solution;
-    switch (expression) {
-        case document.getElementById('addition').clicked === true:
-            solution = parseInt(variable1) + parseInt(variable2);    // perform an addition
-            document.getElementById("solution").addEventListener("click", function() {
-                alert("Het antwoord is : " + solution);
-                location.reload()
-            });
-
-            //Statements executed when the
-            //result of expression matches value1
+    switch (test){
+        case 'addition':
+            result = parseInt(number1) + parseInt(number2);
+            alert (result);
             break;
-        case document.getElementById('subtraction').clicked === true:
-            solution = parseInt(variable1) - parseInt(variable2);   // perform an substraction
-            document.getElementById("solution").addEventListener("click", function() {
-                alert("Het antwoord is : " + solution);
-                location.reload()
-            });//Statements executed when the
-            //result of expression matches value2
+        case 'subtraction':
+            result = parseInt(number1) - parseInt(number2);
+            alert (result);
             break;
-        case document.getElementById('multiplication').clicked === true:
-                solution = parseInt(variable1 * variable2);    // perform an multiplication
-                document.getElementById("solution").addEventListener("click", function() {
-                    alert("Het antwoord is : " + solution);
-                    location.reload()
-                });
-            //Statements executed when
-            //result of expression matches valueN
+        case 'multiplication':
+            result = parseInt(number1) * parseInt(number2);
+            alert (result);
             break;
-        case document.getElementById('division').clicked === true:
-            solution = parseFloat(variable1) / parseFloat(variable2);    // perform an division
-            document.getElementById("solution").addEventListener("click", function() {
-                alert("Het antwoord is : " + solution);
-                location.reload()
-            });
+        case 'division':
+            result = parseInt(number1) / parseInt(number2);
+            alert (result);
             break;
     }
-            //Statements executed when
-            //result of expression matches valueN
+
 
 
 }
-
-
-(function() {
-    return true;
-})();
