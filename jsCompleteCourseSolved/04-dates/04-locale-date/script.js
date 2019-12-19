7aa1a81bb86319d875638c3640c845a8c79d3bb4
JensDN <jens.deneef@gmail.com>
2019-12-19 14:09:40 +0100

    I gave sicko's link/tip a try, but i want to invext more time in the other exercises, i will go back to this one later.

---
+++ jsCompleteCourseSolved/04-dates/04-locale-date/script.js
@@ -0,0 +1,32 @@
+/* becode/javascript
+ *
+ * /04-dates/04-locale-date/script.js - 4.4: date textuelle
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+    var dt = new Date();
+
+
+
+    var options = {
+
+        hour12: false,
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric'
+    };
+
+
+    document.getElementById("target").innerHTML = dt.toLocaleString('en-GB', options);
+
+
+})();
