7c4a1da84f7381ef9eca4b60146ba1338a8e6a49
JensDN <jens.deneef@gmail.com>
2019-12-23 15:17:46 +0100

    Oefening 6.5 done

---
+++ jsCompleteCourseSolved/06-dom/05-hover-image/script.js
@@ -0,0 +1,23 @@
+/* becode/javascript
+ *
+ * /06-dom/05-hover-image/script.js - 6.5: survol d'image
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    // your code here
+    document.getElementsByTagName('img')[0].addEventListener("mouseover", () => {
+        let newSrc = document.getElementsByTagName('img')[0].getAttribute("data-hover");
+        console.log(newSrc);
+        document.getElementsByTagName("img")[0].setAttribute("src", newSrc);
+    });
+
+
+
+})();
