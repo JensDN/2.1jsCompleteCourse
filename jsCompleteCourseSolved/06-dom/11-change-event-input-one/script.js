21e61b61d6cb688b835a6e60f82d0d4471ea962c
JensDN <jens.deneef@gmail.com>
2019-12-24 10:41:47 +0100

    Oefening 6.11 done

---
+++ jsCompleteCourseSolved/06-dom/11-change-event-input-one/script.js
@@ -0,0 +1,20 @@
+/* becode/javascript
+ *
+ * /06-dom/11-change-event-input-one/script.js - 6.11: événement change (1)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+
+    function setMaxLength () {
+        var input = document.getElementById ("pass-one");
+        input.maxLength = 10;
+    }
+    setMaxLength();
+})();
