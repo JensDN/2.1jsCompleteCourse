f36188b6dfb6bd85131f3ff3d07f8e8ade8bbab5
JensDN <jens.deneef@gmail.com>
2019-12-31 12:18:28 +0100

    Aiaiai

---
+++ jsCompleteCourseSolved/07-misc/05-worst-ui-three/script.js
@@ -0,0 +1,21 @@
+/* becode/javascript
+ *
+ * /07-misc/05-worst-ui-three/script.js - 7.5: la pire interface (3) : phone slot
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+   var random3 = setInterval(function(){
+       counter ++ 
+       var a = Math.floor(Math.random()*10);
+       var b = Math.floor(Math.random()*10);
+       var c = Math.floor(Math.random()*10);
+   })
+
+})();
